5fa0d047a6c569b5c654bec498454728e503ad8b
Javi Rudolph <javiera.av@ufl.edu>
2021-08-17 11:30:56 -0400

    documentation for the environment data

---
+++ R/orig-no-seed-MEMsel.R
@@ -0,0 +1,5 @@
+#' Selected MEMs for 1000 sites
+#'
+#' This dataset has the 78 variables used in the spatial component of the analysis, significant variables from Moran Eigenvector Maps used in analysis in the 2021 paper, 'The Internal Structure of Metacommunities'
+#'
+"orig-no-seed-MEMsel"
---
+++ R/orig-no-seed-XY.R
@@ -0,0 +1,5 @@
+#' Coordinates variable for 1000 sites
+#'
+#' This data has the xy coordinates used for the simulations and analysis in the 2021 paper, 'The Internal Structure of Metacommunities'
+#'
+"orig-no-seed-XY"
