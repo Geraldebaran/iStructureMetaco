e5e9fcbaa650ee9a0ddaad8caa8b110dd6918a6d
Javi Rudolph <javiera.av@ufl.edu>
2021-08-17 11:19:42 -0400

    Added data and documentation for the environmental variable and coordinates used in our analysis

---
+++ R/E.R
@@ -0,0 +1,5 @@
+#' Environmental variable for 1000 sites
+#'
+#' This vector contains the environmental variable used for the simulations and analysis in the 2021 paper, 'The Internal Structure of Metacommunities'
+#'
+"E"
